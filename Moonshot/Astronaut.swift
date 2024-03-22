2fe055a1cb524d6be1a2480ea327b4f9c8bd882c
Jarrod Merlin <jarrod.merlin@stu.barren.kyschools.us>
2024-03-22 08:14:00 -0500

    initial commit, file error project

---
+++ Moonshot/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Austronaut.swift
+//  Moonshot
+//
+//  Created by Student on 3/14/24.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
